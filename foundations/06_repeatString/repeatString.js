35bd39f9351439a28f238478784acc1ca3944cb6
ottoode <229023441+ottoode@users.noreply.github.com>
2026-04-07 23:29:28 +0200

    Complete exercise 06_repeatString

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0) return "ERROR";
+    let combinedString = "";
+    for (let i = 0; i < num; i++) {
+        combinedString += string;
+    }
+    return combinedString;
 };
 
 // Do not edit below this line
