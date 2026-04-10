224a74862f9658e122a787a6ffea399c5fb8c68f
ottoode <229023441+ottoode@users.noreply.github.com>
2026-04-10 18:32:50 +0200

    Complete exercise 14_fibonacci

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+    if (typeof n === 'string') {
+        n = Number(n);
+    }
+    if (n === 0) return 0;
+    if (n < 0) return "OOPS"
 
+
+    let number1 = 1;
+    let number2 = 0;
+
+    for (let i = 0; i < n; i++) {
+        let temp = number1 + number2;
+        number1 = number2;
+        number2 = temp;
+    }
+    return number2;
 };
 
 // Do not edit below this line
